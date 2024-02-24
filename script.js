96111b41f2f9d635ce31ff942410692b686d263c
BassTons <156529112+BassTons@users.noreply.github.com>
2024-02-24 16:02:43 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-/*INIZIO DI SCENEGGIATURA*/
-//RECUPERO PRIMORDIALE GUADAGNI
-var articolo = document.querySelectorAll('.articolo');
-var PadreArticolo = document.querySelector('.padre');
-var prezzoTotale = document.getElementById('totale-prezzo'); 
+/*DEBUT DU SCRIPT*/
+//RECUPERATION DES PREMIERS ELEMENTS ESSENTIELS
+var article = document.querySelectorAll('.item');
+var ParentArticle = document.querySelector('.parent');
+var prixTotal = document.getElementById('total-price'); 
 
-//CRESCITA , DIMINUZIONE , RIMOZIONE ET FAVORITO
-for (var i = 0; i < articolo.length; i++) {
+//AUGMENTATION , DIMINUTION , SUPRESSION ET FAVORITE
+for (var i = 0; i < article.length; i++) {
 
-//RECUPERO DA PULSANTI E DA PREZZO
-    let meno = articolo[i].children[1]; //PUSANTE MENO
-    let piu = articolo[i].children[3]; // PUSANTE PIU
-    let btnDelete = articolo[i].children[4];// PUSANTE RIMOZIONE
-    let articolonum = articolo[i].children[2];//LETTORI D'ARTICOLO
-    let quantite = parseInt(articolonum.innerText);//LA QUANTITE
-    let prezzo = articolo[i].children[7].children[0];//PRIX TOTAL DES ARTICLES
-    let prezzoUnità = articolo[i].children[6];//PRIX UNITAIRE
-    let ArticoloPrezzo = parseInt(prixUnit.innerText);//PRIX DE L'ARTICLE EN TEXTE
-    let Fav = articolo[i].children[5];//PUSANTE PREFERITI
-}
+//RECUPERATION DES BOUTONS ET DES PRIX
+    let moins = article[i].children[1]; //BOUTONS MOINS
+    let plus = article[i].children[3]; // BOUTONS PLUS
+    let btnDelete = article[i].children[4];// BOUTON DELETE
+    let articleNum = article[i].children[2];//NOMBRE D'ARTICLE
+    let quantity = parseInt(articleNum.innerText);//LA QUANTITE
+    let prix = article[i].children[7].children[0];//PRIX TOTAL DES ARTICLES
+    let prixUnit = article[i].children[6];//PRIX UNITAIRE
+    let ArticlePrice = parseInt(prixUnit.innerText);//PRIX DE L'ARTICLE EN TEXTE
+    let Fav = article[i].children[5];//BOUTON FAVORIS
+
 
 //DECRIMENTER LE NOMBRE D'ARTICLE ET METTRE A JOUR LE PRIX
-    meno.addEventListener('click', function () {
-        if (quantite > 0) {
-            quantite--;
-            articolonum.innerHTML = quantità;
-            prezzo.innerText = quantite * ArticoloPrezzo;
+    moins.addEventListener('click', function () {
+        if (quantity > 0) {
+            quantity--;
+            articleNum.innerHTML = quantity;
+            prix.innerText = quantity * ArticlePrice;
             calSum(); 
         }
     });
 
 
 //AUGMENTER LE NOMBRE D'ARTICLE ET METTRE A JOUR
-    piu.addEventListener('click', function () {
-        quantità++;
-        articoloNum.innerHTML = quantità;
-        prezzo.innerText = quantità * ArticoloPrezzo;
+    plus.addEventListener('click', function () {
+        quantity++;
+        articleNum.innerHTML = quantity;
+        prix.innerText = quantity * ArticlePrice;
         calSum(); 
     });
 
-//ELIMINARE UN ARTICOLO E METTERE A GIORNO
+//SUPPRIMER UN ARTICLE ET METTRE A JOUR
     btnDelete.addEventListener('click', function (e) {
-        e.target.padreElement.remove();
+        e.target.parentElement.remove();
         calSum(); // Mettez à jour le total à chaque suppression
     });
 
-//AGGIUNTA IN PREFERITI E CAMBIAMENTO DI COLORE
-    const colors = ['red', 'transparent']; // Listes de colore
+//AJOUT EN FAVORIS ET CHANGEMENT DE COULEUR
+    const colors = ['red', 'transparent']; // Liste de couleurs
     let currentIndex = 0;
 
     Fav.addEventListener('click', function () {
@@ -53,19 +53,18 @@ for (var i = 0; i < articolo.length; i++) {
         currentIndex = (currentIndex + 1) % colors.length;
         return alert(`Added to favorite`)
     });
-
-
+}
 
 //CALCUL DES PRIX TOTAUX
 function calSum() {
-    let totale = 0;
+    let total = 0;
 
     // Parcourez chaque article pour calculer le total
-    for (var i = 0; i < articolo.length; i++) {
-        totale += parseInt(articolo[i].querySelector('.prezzo').innerText.replace('$', '')) * parseInt(articolo[i].querySelector('.quantità').innerText);
+    for (var i = 0; i < article.length; i++) {
+        total += parseInt(article[i].querySelector('.price').innerText.replace('$', '')) * parseInt(article[i].querySelector('.quantity').innerText);
     }
 
-    prezzoTotale.textContent = totale.toFixed(2) + ' $'; 
+    prixTotal.textContent = total.toFixed(2) + ' $'; 
    
    
 }
